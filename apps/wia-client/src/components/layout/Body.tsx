8d524a755a45e1367d14a4bd77e195b0aa8f4c10
Jonathan Nunez <jonathannunezr1@gmail.com>
2023-09-19 13:11:50 -0600

    feat (chakra): working on wia-client

---
+++ apps/wia-client/src/components/layout/Body.tsx
@@ -0,0 +1,26 @@
+import { Box } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+interface IBodyProps {
+	children: ReactNode;
+	v?: boolean;
+	h?: boolean;
+}
+
+function Body({ children, h, v }: IBodyProps) {
+	return (
+		<Box
+			display='flex'
+			alignItems={v ? 'center' : undefined}
+			justifyContent={h ? 'center' : undefined}
+			minHeight='60vh'
+			gap={8}
+			mb={8}
+			w='full'
+		>
+			{children}
+		</Box>
+	);
+}
+
+export default Body;
---
+++ apps/wia-client/src/home/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/react';
+import Body from '../components/layout/Body';
+
+function Home() {
+	return (
+		<Body v h>
+			<Box>
+				<Heading>welcome user</Heading>
+			</Box>
+		</Body>
+	);
+}
+
+export default Home;
