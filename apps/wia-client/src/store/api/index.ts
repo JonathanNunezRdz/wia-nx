a316d7b1bc6e1d986af0b3f2fbfa0e4eb690b79e
Jonathan Nunez <jonathannunezr1@gmail.com>
2023-09-21 18:13:15 -0600

    updated baseUrl in client

--- apps/wia-client/src/store/api/index.ts
+++ apps/wia-client/src/store/api/index.ts
@@ -1,6 +1,17 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3333';
+function getBaseUrl() {
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+	if (!baseUrl) {
+		console.error(
+			'NEXT_PUBLIC_BASE_URL not available in environment, setting default localhost'
+		);
+		return 'http://localhost:3333';
+	}
+	return baseUrl;
+}
+
+const BASE_URL = getBaseUrl();
 
 const axiosConfig: AxiosRequestConfig<void> = {
 	baseURL: `${BASE_URL}/api`,
