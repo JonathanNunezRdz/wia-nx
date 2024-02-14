48f88ab69a6c76b41d2f674b8a7c3d954aa25716
Jonathan Nunez <jonathannunezr1@gmail.com>
2024-02-14 15:28:03 -0600

    fixed http tp https

--- apps/wia-server/src/main.ts
+++ apps/wia-server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 	app.enableCors({
 		origin: [
 			'http://localhost:4200',
-			'http://wia-nx-production.up.railway.app',
+			'https://wia-nx-production.up.railway.app',
 		],
 	});
 
